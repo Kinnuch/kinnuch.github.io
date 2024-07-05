a262a220a964b9a945bba5dd1256baef1a52b614
Kinnuch <1650229968@qq.com>
2024-07-05 17:44:57 +0800

    update cerf test

--- assets/js/prestanneth.js
+++ assets/js/prestanneth.js
@@ -1,17 +1,27 @@
 
-function main() {
-    const FileURL = "https://kinnuch.github.io/file/prestanneth.csv";
+function getRandomLine(inDataArray) {
+    const randomIndex = Math.floor(Math.random() * inDataArray.length);
+    const randomLine = inDataArray[randomIndex];
+    return randomLine;
+}
 
-    fetch(FileURL)
-        .then(response => response.text())
-        .then(csvData => {
-            var csvHtml = csvData.trim().split('\n').map(line => line.split(','));
-            var csvBody = document.getElementById('Prestanneth');
-            csvBody.innerHTML = csvHtml;
-        })
-        .catch(error => {
-            console.error("ú-'resta", error);
-        });
+function updateLine(inDataArray) {
+    const randomLine = getRandomLine(inDataArray);
+    document.getElementById('Prestanneth').innerHTML = randomLine;
+}
+
+async function main() {
+    const FileURL = "https://kinnuch.github.io/file/prestanneth.csv";
+    try {
+        const response = await fetch(FileURL);
+        const csvData = await response.text();
+        const dataArray = csvData.trim().split('\n').map(line => line.split(','));
+        updateLine(dataArray);
+        document.getElementById('refreshButton').addEventListener('click', updateLine(dataArray));
+    }
+    catch (error){
+        console.error("ú-'resta", error);
+    }
 }
 
 main()
--- cerf.md
+++ cerf.md
@@ -10,6 +10,8 @@ title: Cerf
 
   <div id="Prestanneth"></div>
 
+  <button id="refreshButton">Laeda 刷新</button>
+
   <script src="https://kinnuch.github.io/assets/js/prestanneth.js"></script>
 
 - 

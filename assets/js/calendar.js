bd0bc4a8cc3b0c9f2f88fd1636c94131e8d17407
wuyichen <1650229968@qq.com>
2026-05-25 16:36:19 +0800

    update Calender module

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -5,187 +5,146 @@ var MandarinSeason = ["新年", "春季", "夏季", "秋季", "年半", "落季"
 var MandarinDay = ["星辰日", "太阳日", "月亮日", "双树日", "天穹日", "维拉日"];
 var MandarinWeek = ["一", "二", "三", "四", "五", "六", "七", "八", "九", "十", "十一", "十二"];
 
-function showCalendar(Year, Month, Day){
+var SeasonDays = [1, 54, 72, 54, 3, 54, 72, 54, 1];
+
+function getSeason(Year, Month, Day) {
     var Season = -1;
-    var Ac = -1;
-    var SDay = -1;
-    var SWeek = -1;
-    var SYear;
-    var sy = String(Year);
-    SYear = sy[3] + sy[2] + sy[1] + sy[0];
     switch(Month){
-        case 1:{
-            if (Day >= 27) {Season = 7;}
-            else {Season = 6;}
+        case 1: Season = (Day >= 27) ? 7 : 6; break;
+        case 2: Season = (Day == 29) ? 9 : 7; break;
+        case 3:
+            if (Day <= 21) Season = 7;
+            else if (Day == 22) Season = 8;
+            else if (Day == 23) Season = 0;
+            else Season = 1;
             break;
-        }
-        case 2:{
-            if (Day == 29) {Season = 9;}
-            else {Season = 7;} 
+        case 4: Season = 1; break;
+        case 5: Season = (Day <= 16) ? 1 : 2; break;
+        case 6: Season = 2; break;
+        case 7: Season = (Day <= 27) ? 2 : 3; break;
+        case 8: Season = 3; break;
+        case 9:
+            if (Day <= 19) Season = 3;
+            else if (Day <= 22) Season = 4;
+            else Season = 5;
             break;
-        }
-        case 3:{
-            if (Day <= 21) {Season = 7;}
-            else{
-                if (Day == 22) {Season = 8;}
-                else{
-                    if (Day == 23) {Season = 0;}
-                    else {Season = 1;}
-                }
-            }
-            break;
-        }
-        case 4:{Season = 1; break;}
-        case 5:{
-            if (Day <= 16) {Season = 1;}
-            else {Season = 2;}
-            break;
-        }
-        case 6:{Season = 2;break;}
-        case 7:{
-            if (Day <= 27) {Season = 2;}
-            else {Season = 3;}
-            break;
-        }
-        case 8:{Season = 3; break;}
-        case 9:{
-            if (Day <= 19) {Season = 3;}
-            else{
-                if (Day > 19 && Day <= 22) {Season = 4;}
-                else {Season = 5;}
-            }
-            break;
-        }
-        case 10:{Season = 5; break;}
-        case 11:{
-            if (Day <= 15) {Season = 5;}
-            else {Season = 6;}
-            break;
-        }
-        case 12:{Season = 6; break;}
+        case 10: Season = 5; break;
+        case 11: Season = (Day <= 15) ? 5 : 6; break;
+        case 12: Season = 6; break;
     }
-    if (Season != 0 && Season != 4 && Season != 8 && Season != 9){
-        switch(Season){
-            case 1:{
-                switch(Month){
-                    case 3: Ac = Day - 24;
-                    break;
-                    case 4: Ac = Day + 7;
-                    break;
-                    case 5: Ac = Day + 37;
-                    break;
-                }
-                break;
-            }
-            case 2:{
-                switch(Month){
-                    case 5: Ac = Day - 17;
-                    break;
-                    case 6: Ac = Day + 14;
-                    break;
-                    case 7: Ac = Day + 44;
-                    break;
-                }
-                break;
-            }
-            case 3:{
-                switch(Month){
-                    case 7: Ac = Day - 28;
-                    break;
-                    case 8: Ac = Day + 3;
-                    break;
-                    case 9: Ac = Day + 34;
-                    break;
-                }
-                break;
-            }
-            case 5:{
-                switch(Month){
-                    case 9: Ac = Day - 23;
-                    break;
-                    case 10: Ac = Day + 7;
-                    break;
-                    case 11: Ac = Day + 38;
-                    break;
-                }
-                break;
-            }
-            case 6:{
-                switch(Month){
-                    case 11: Ac = Day - 16;
-                    break;
-                    case 12: Ac = Day + 14;
-                    break;
-                    case 1: Ac = Day + 45;
-                    break;
-                }
-                break;
-            }
-            case 7:{
-                switch(Month){
-                    case 1: Ac = Day - 27;
-                    break;
-                    case 2: Ac = Day + 4;
-                    break;
-                    case 3: Ac = Day + 32;
-                    break;
-                }
-                break;
-            }
-        }
+    return Season;
+}
+
+function getAc(Season, Month, Day) {
+    var Ac = -1;
+    if (Season == 0 || Season == 4 || Season == 8 || Season == 9) return Ac;
+    switch(Season){
+        case 1:
+            if (Month==3) Ac=Day-24; else if(Month==4) Ac=Day+7; else if(Month==5) Ac=Day+37;
+            break;
+        case 2:
+            if (Month==5) Ac=Day-17; else if(Month==6) Ac=Day+14; else if(Month==7) Ac=Day+44;
+            break;
+        case 3:
+            if (Month==7) Ac=Day-28; else if(Month==8) Ac=Day+3; else if(Month==9) Ac=Day+34;
+            break;
+        case 5:
+            if (Month==9) Ac=Day-23; else if(Month==10) Ac=Day+7; else if(Month==11) Ac=Day+38;
+            break;
+        case 6:
+            if (Month==11) Ac=Day-16; else if(Month==12) Ac=Day+14; else if(Month==1) Ac=Day+45;
+            break;
+        case 7:
+            if (Month==1) Ac=Day-27; else if(Month==2) Ac=Day+4; else if(Month==3) Ac=Day+32;
+            break;
     }
+    return Ac;
+}
+
+function showCalendar(Year, Month, Day){
+    var Season = getSeason(Year, Month, Day);
+    var Ac = getAc(Season, Month, Day);
+    var SDay = -1, SWeek = -1;
+    var sy = String(Year);
+    var SYear = sy[3] + sy[2] + sy[1] + sy[0];
+
     if (Ac != -1){
         SDay = Ac % 6;
         SWeek = (Ac - SDay) / 6;
     }
-    var tsHtml;
-    if (Season != 0 && Season != 4 && Season != 8 && Season != 9){
-        tsHtml = "<td>" + SindarinDay[SDay] + "</td>";
-        tsHtml += "<td>Odlad " + SindarinWeek[SWeek] + "</td>";
-    }
-    else{
-        if (Season == 0 || Season == 8){tsHtml = "";}
-        if (Season == 4){tsHtml = "<td>Arad " + SindarinWeek[Day - 20] + "</td>";}
-        if (Season == 9){
-            var leap = ((Year % 100) % 12) / 4;
-            if (leap == 0) leap = 3;
-            tsHtml = "<td>Arad " + SindarinWeek[2 + leap] + "</td>";
+
+    var totalDays = SeasonDays[Season] || 1;
+    var weeks = Math.ceil(totalDays / 6);
+
+    var html = '<div class="cal-header">';
+    html += '<span class="cal-season">' + SindarinSeason[Season] + ' / ' + MandarinSeason[Season] + '</span>';
+    html += '<span class="cal-year">' + SYear + '</span>';
+    html += '</div>';
+
+    if (totalDays > 1 && Season != 4 && Season != 9) {
+        html += '<table class="cal-grid"><thead><tr>';
+        for (var d = 0; d < 6; d++) {
+            html += '<th title="' + MandarinDay[d] + '">' + SindarinDay[d].substring(0,3) + '</th>';
         }
-    }
-    if (Season != 9) {tsHtml += "<td>" + SindarinSeason[Season] + "</td>";}
-    else {
-        var leapYear = ((Year % 100) % 12);
-        if (leapYear != 0) leapYear = Year + 12 - leapYear;
-        else leapYear = Year;
-        var sly = String(leapYear);
-        tsHtml += "<td>Amin " + sly[3] + sly[2] + sly[1] + sly[0] + "</td>";
-    }
-    tsHtml += "<td>" + SYear + "</td>";
-    var tsbody = document.getElementById('Sindarin');
-    tsbody.innerHTML = tsHtml;
-    var tmHtml = "<td>" + String(Year) + "年</td>";
-    if (Season != 9) {tmHtml += "<td>" + MandarinSeason[Season] + "</td>";}
-    else {
-        var leapYear = ((Year % 100) % 12);
-        if (leapYear != 0) leapYear = Year + 12 - leapYear;
-        else leapYear = Year;
-        var sly = String(leapYear);
-        tmHtml += "<td>闰于" + sly[3] + sly[2] + sly[1] + sly[0] + "年年半</td>";
-    }
-    if (Season != 0 && Season != 4 && Season != 8 && Season != 9){
-        tmHtml += "<td>第" + MandarinWeek[SWeek] + "周</td>";
-        tmHtml += "<td>" + MandarinDay[SDay] + "</td>";
-    }
-    else{
-        if (Season == 4){tmHtml += "<td>第" + MandarinWeek[Day - 20] + "天</td>";}
-        if (Season == 9){
-            var leap = ((Year % 100) % 12) / 4;
-            if (leap == 0) leap = 3;
-            tmHtml += "<td>第" + MandarinWeek[2 + leap] + "天(闰)</td>";
+        html += '</tr></thead><tbody>';
+        for (var w = 0; w < weeks; w++) {
+            html += '<tr>';
+            for (var d = 0; d < 6; d++) {
+                var dayNum = w * 6 + d;
+                if (dayNum < totalDays) {
+                    var isToday = (w === SWeek && d === SDay);
+                    var cls = isToday ? ' class="cal-today"' : '';
+                    var weekLabel = MandarinWeek[w] + '周' + MandarinDay[d];
+                    html += '<td' + cls + ' title="' + weekLabel + '"><span class="cal-day-num">' + (dayNum+1) + '</span></td>';
+                } else {
+                    html += '<td></td>';
+                }
+            }
+            html += '</tr>';
         }
+        html += '</tbody></table>';
+    } else if (Season == 4) {
+        html += '<div class="cal-special">';
+        for (var i = 0; i < 3; i++) {
+            var isToday = (Day - 20 === i);
+            var cls = isToday ? ' cal-today' : '';
+            html += '<span class="cal-special-day' + cls + '">Arad ' + MandarinWeek[i] + '</span>';
+        }
+        html += '</div>';
+    } else {
+        var label = (Season == 0) ? 'Orvinui / 新年' : (Season == 8) ? 'Penninor / 除夕' : 'Penninor / 闰日';
+        html += '<div class="cal-special"><span class="cal-special-day cal-today">' + label + '</span></div>';
     }
-    var tmbody = document.getElementById('Mandarin');
-    tmbody.innerHTML = tmHtml;
+
+    html += '<div class="cal-detail" id="cal-detail" style="display:none;">';
+    if (SDay != -1) {
+        html += '<p>' + SindarinDay[SDay] + ' / ' + MandarinDay[SDay] + '</p>';
+        html += '<p>Odlad ' + SindarinWeek[SWeek] + ' / 第' + MandarinWeek[SWeek] + '周</p>';
+    } else if (Season == 4) {
+        html += '<p>Arad ' + MandarinWeek[Day - 20] + ' / 年半第' + MandarinWeek[Day - 20] + '天</p>';
+    } else if (Season == 9) {
+        var leap = ((Year % 100) % 12) / 4;
+        if (leap == 0) leap = 3;
+        html += '<p>Arad ' + SindarinWeek[2 + leap] + ' / 闰日</p>';
+    }
+    html += '<p>' + SindarinSeason[Season] + ' ' + SYear + '</p>';
+    html += '</div>';
+
+    var frame = document.getElementById('CalendarFrame');
+    frame.innerHTML = html;
+
+    var todayCells = frame.querySelectorAll('.cal-today');
+    var detail = document.getElementById('cal-detail');
+    for (var i = 0; i < todayCells.length; i++) {
+        todayCells[i].style.cursor = 'pointer';
+        todayCells[i].addEventListener('click', function() {
+            if (detail.style.display === 'none') {
+                detail.style.display = 'block';
+            } else {
+                detail.style.display = 'none';
+            }
+        });
+    }
 }
 
 var curDate = new Date();
